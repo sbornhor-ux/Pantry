ab61f43df84c64d5cea3ac5a4baa26943aabca80
sbornhor-ux <sbornhor@uchicago.edu>
2026-03-03 12:30:32 -0600

    V3

--- sw.js
+++ sw.js
@@ -1,24 +1,7 @@
-var CACHE_NAME = 'pantry-v2';
-var ASSETS = ['./', './index.html', './manifest.json'];
-
-self.addEventListener('install', function(e) {
-  e.waitUntil(caches.open(CACHE_NAME).then(function(c) { return c.addAll(ASSETS); }));
-  self.skipWaiting();
-});
-
-self.addEventListener('activate', function(e) {
-  e.waitUntil(caches.keys().then(function(keys) {
-    return Promise.all(keys.filter(function(k) { return k !== CACHE_NAME; }).map(function(k) { return caches.delete(k); }));
-  }));
-  self.clients.claim();
-});
-
+self.addEventListener('install', function() { self.skipWaiting(); });
+self.addEventListener('activate', function() { self.clients.claim(); });
 self.addEventListener('fetch', function(e) {
-  e.respondWith(
-    fetch(e.request).catch(function() {
-      return caches.match(e.request).then(function(r) {
-        return r || caches.match('./index.html');
-      });
-    })
-  );
+  e.respondWith(fetch(e.request).catch(function() {
+    return caches.match(e.request);
+  }));
 });
